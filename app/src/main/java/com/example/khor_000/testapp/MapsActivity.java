deb0223e46206b4b384c40044a5608504f10ffb0
Teri <chh115@ucsd.edu>
2016-05-08 15:16:05 -0700

    3:15 May 8

--- app/src/main/java/com/example/khor_000/testapp/MapsActivity.java
+++ app/src/main/java/com/example/khor_000/testapp/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.khor_000.testapp;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -7,6 +8,8 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.provider.SyncStateContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -21,6 +24,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -42,7 +48,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ArrayAdapter<LocItem> arrayAdapter;
     private ArrayList<String> arrayList;
     private ListView listView;
+    private Marker addFavor;
     private List<LocItem> myLocations = new ArrayList<LocItem>();
+    private List<Marker> locMakers = new ArrayList<Marker>();
+    private String pastLocation = "";
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -64,14 +79,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         listView = (ListView) findViewById(R.id.lv);
         listView.setAdapter(arrayAdapter);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private class MyLocAdapter extends ArrayAdapter<LocItem>{
-        public MyLocAdapter(){
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.khor_000.testapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.khor_000.testapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    private class MyLocAdapter extends ArrayAdapter<LocItem> {
+        public MyLocAdapter() {
             super(MapsActivity.this, R.layout.single_location, myLocations);
         }
 
-        public void add(LocItem newLoc){
+        public void add(LocItem newLoc) {
             super.add(newLoc);
             super.notifyDataSetChanged();
         }
@@ -122,10 +180,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Add a marker in Sydney and move the camera
-        LatLng sanDiego = new LatLng( 32.7157, -117.1611);
-        final Marker addFavor = mMap.addMarker(new MarkerOptions().position(sanDiego).title("where am I  "));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sanDiego));
+
+        // Add a marker in San Diego and move the camera
+        LatLng sanDiego = new LatLng(32.7157, -117.1611);
+        addFavor = mMap.addMarker(new MarkerOptions().position(sanDiego).title(" Where Am I "));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sanDiego));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(sanDiego.latitude, sanDiego.longitude), 12.0f));
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
@@ -138,7 +198,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        mMap.setOnMapClickListener( new GoogleMap.OnMapClickListener() {
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(final LatLng point) {
 
@@ -146,66 +206,84 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 showList.setVisibility(View.VISIBLE);
                 //  show the alert box
                 addFavor.setPosition(point);
-                setLocat = LayoutInflater.from(MapsActivity.this).inflate(R.layout.set_location,null);
+                setLocat = LayoutInflater.from(MapsActivity.this).inflate(R.layout.set_location, null);
                 AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
                 builder.setMessage("Assign A Name To This Location");
-                final EditText favName =(EditText) setLocat.findViewById(R.id.nameOfLocation);
+                final EditText favName = (EditText) setLocat.findViewById(R.id.nameOfLocation);
 
-                builder.setView(setLocat).setPositiveButton("Add",new DialogInterface.OnClickListener(){
+                builder.setView(setLocat).setPositiveButton("Add", new DialogInterface.OnClickListener() {
 
-                   public void onClick(DialogInterface dialog, int which){
-                       // set it to Data - base
-                       locationName = favName.getText().toString();
+                    public void onClick(DialogInterface dialog, int which) {
+                        // set it to Data - base
+                        locationName = favName.getText().toString();
 
-                       if( !locationName.isEmpty() ) {
+                        if (!locationName.isEmpty()) {
 
-                           //save location with given info
-                           LocItem tempLoc = new LocItem( locationName, point.latitude, point.longitude );
+                            //save location with given info
+                            LocItem tempLoc = new LocItem(locationName, point.latitude, point.longitude);
 
-                           arrayAdapter.add(tempLoc);
+                            arrayAdapter.add(tempLoc);
 
-                           Toast.makeText(getApplicationContext(), "Added To Favorite", Toast.LENGTH_SHORT).show();
-                       }
+                            //add a marker on the map
+                            addFavor = mMap.addMarker(new MarkerOptions().position(point).title(locationName));
+                            locMakers.add(addFavor);
 
-                       else {
-                           Toast.makeText(getApplicationContext(), "No Input Detected", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Added To Favorite", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "No Input Detected", Toast.LENGTH_SHORT).show();
+                            addFavor.remove();
 
-                       }
+                        }
 
-                   }                    // close the prompt after click cancel
-                }).setNegativeButton("Cancel",null).setCancelable(true);  // cancelable even using back key
+                    }                    // close the prompt after click cancel
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_SHORT).show();
+                        //addFavor.remove();
+
+                    }                    // close the prompt after click cancel
+                }).setCancelable(true);  // cancelable even using back key
 
                 AlertDialog alert = builder.create();
                 alert.show();
             }
         });
 
-        LocationListener locationListener  = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    //check if location is in the range of any of the favorite locations
-                    for (LocItem i: myLocations){
-                        if(getRange(location.getLatitude(), location.getLongitude(), i.getLatitude(), i.getLongtitude()) < 160.9 ){
-                            //sendSMS(i);
-                            Toast.makeText(getApplicationContext(), "Visted!", Toast.LENGTH_SHORT).show();
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                //check if location is in the range of any of the favorite locations
+                for (LocItem i : myLocations) {
+                    if (getRange(location.getLatitude(), location.getLongitude(), i.getLatitude(), i.getLongtitude()) < 160.9) {
+
+                        // only send msg if visit different fav location
+                        if ( pastLocation != i.getName()) {
+                            Toast.makeText(getApplicationContext(), "Vist: " + i.getName(), Toast.LENGTH_SHORT).show();
+                             pastLocation = i.getName();
                         }
                     }
                 }
 
-                public double getRange(double lat1, double lon1, double lat2, double lon2){
-                    Double R = 6378.137; // Radius of earth in KM
-                    Double dLat = (lat2 - lat1) * Math.PI / 180;
-                    Double dLon = (lon2 - lon1) * Math.PI / 180;
+                mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()) , 16.0f) );
 
-                    Double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-                                    Math.sin(dLon/2) * Math.sin(dLon/2);
+            }
 
-                    Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-                    Double d = R * c;
-                    d =  d * 1000; // meters
-                    return d;
-                }
+            public double getRange(double lat1, double lon1, double lat2, double lon2) {
+                Double R = 6378.137; // Radius of earth in KM
+                Double dLat = (lat2 - lat1) * Math.PI / 180;
+                Double dLon = (lon2 - lon1) * Math.PI / 180;
+
+                Double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+                                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+                Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                Double d = R * c;
+                d = d * 1000; // meters
+                return d;
+            }
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -226,8 +304,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         String locationProvider = LocationManager.GPS_PROVIDER;
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED  ){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -235,12 +313,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
 
-            Log.d("test1","ins");
+            Log.d("test1", "ins");
             return;
 
-        }else if (mMap != null){
+        } else if (mMap != null) {
             Log.d("test2", "outs");
             mMap.setMyLocationEnabled(true);
         }
